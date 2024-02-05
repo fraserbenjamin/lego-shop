1ed70f21b31a0ee1e8fd9af063ec0430b1f8cf7a
Fraser Benjamin <fraser.benjamin@outlook.com>
2024-02-05 19:41:27 +0000

    Fixed responsive issue

--- src/components/TelephoneBoxHero.tsx
+++ src/components/TelephoneBoxHero.tsx
@@ -3,18 +3,18 @@ import telephoneBoxMotionHero from "../assets/telephone-box-motion-hero.mp4";
 const TelephoneBoxHero = () => {
 
   return (
-    <div className="w-full relative flex place-items-center bg-neutral-900 min-h-64">
+    <div className="w-full relative flex place-items-center bg-neutral-900 min-h-64 h-64 sm:h-auto">
       <div className="flex flex-col text-white absolute z-10 place-items-start space-y-5 max-w-lg mx-24">
         <h1 className="text-5xl">London calling</h1>
-        <p className="text-lg">Recreate a British cultural icon with the new Red London Telephone Box.</p>
+        <p className="text-lg hidden sm:inline">Recreate a British cultural icon with the new Red London Telephone Box.</p>
 
         <button>Shop Now</button>
       </div>
 
-      <video className="w-full" data-test="video" loop playsInline autoPlay muted>
+      <video className="h-full w-full object-cover" loop playsInline autoPlay muted>
         <source src={telephoneBoxMotionHero} type="video/mp4" />
       </video>
-    </div>
+    </div >
   )
 }
 
