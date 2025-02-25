e01889020cbaec489138a96013227f4a8c2ea7b7
Fraser Benjamin <fraser.benjamin@outlook.com>
2025-02-25 21:51:26 +0000

    Update versions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
+import FeaturedSets from "../components/FeaturedSets";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
 import Placeholder from "../components/Placeholder";
+import PromoBar from "../components/PromoBar";
+import StarWarsHero from "../components/StarWarsHero";
 
 const Home = () => {
   return (
     <>
-      <Placeholder />
+      <PromoBar />
+      <NavBar />
+      <StarWarsHero />
+
+      <FeaturedSets />
+
+      <Footer />
     </>
   );
 };
